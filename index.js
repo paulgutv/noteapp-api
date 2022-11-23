422152752c9c614850d346fdff6deeb38319de2b
Paul Gutierrez <gutierrez.paul@pucp.pe>
2022-11-22 20:16:51 -0500

    error tests

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const Note = require('./models/note')
+const { response } = require('express')
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -12,10 +13,11 @@ const requestLogger = (request, response, next) => {
     next()
 }
 
-app.use(express.json())
-app.use(requestLogger)
 app.use(cors())
 app.use(express.static('build'))
+app.use(express.json())
+app.use(requestLogger)
+
 
 
 app.get('/', (req, res) => {
@@ -28,20 +30,41 @@ app.get('/api/notes', (request, response) => {
     })
   })
 
-app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-        response.json(note)
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+    .then(note => {
+        if (note) {
+            response.json(note)
+        } else {
+            response.status(404).end()
+        }
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.put('/api/notes/:id', (request, response, next) => {
+    const { content, important } = request.body
+
+    Note.findByIdAndUpdate(
+        request.params.id,
+        {content, important},
+        { new: true, runValidators: true, context: 'query' }
+    )
+    .then(updatedNote => {
+        response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
 
     if(body.content === undefined) {
@@ -54,9 +77,11 @@ app.post('/api/notes', (request, response) => {
         date: new Date(),
     })
 
-    note.save().then(savedNote => {
+    note.save()
+    .then(savedNote => {
         response.json(savedNote)
     })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -64,6 +89,21 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if(error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    } else if(error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
+    }
+    next(error)
+}
+
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
